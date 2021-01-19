63a2d631bc24c03d8ce2180a16a25ba4fc5ff264
monizb <46259712+monizb@users.noreply.github.com>
2021-01-19 11:23:53 +0530

    Initial Boilerplate

---
+++ server.js
@@ -0,0 +1,41 @@
+////////////////// Imports //////////////////
+
+const express = require("express");
+const body = require("body-parser");
+const firebase = require("firebase-admin");
+
+/////////////////////////////////////////////
+
+////////////////// Configs //////////////////
+
+const firebaseConfig = {
+    apiKey: process.env.KEY,
+    authDomain: process.env.DOMAIN,
+    projectId: process.env.ID,
+    storageBucket: process.env.BUCKET,
+    messagingSenderId: process.env.SENDER,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENT
+};
+
+const app = express();
+app.use(body.json());
+firebase.initializeApp(firebaseConfig);
+
+/////////////////////////////////////////////
+
+///////////// Global Constants /////////////
+
+const PORT = process.env.PORT || 3000
+
+/////////////////////////////////////////////
+
+
+app.listen(PORT, () => { console.log(`Server listening on port: ${PORT}`) });
+
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
+})
+
+
